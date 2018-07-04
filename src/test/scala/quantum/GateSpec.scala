45f44b59c3f3ceb380ee36856b79c42ed29e8811
logicalguess <logicalguess@gmail.com>
2018-07-04 00:36:04 -0400

    tests

--- src/test/scala/quantum/GateSpec.scala
+++ src/test/scala/quantum/GateSpec.scala
@@ -16,6 +16,27 @@ class GateSpec extends Specification {
 
   "Gates" should {
 
+    "I|0>" in {
+      I[Std](S0)(S0) === Complex.one
+      I[Std](S0)(S1) === Complex.zero
+    }
+
+    "I|1>" in {
+      I[Std](S1)(S0) === Complex.zero
+      I[Std](S1)(S1) === Complex.one
+    }
+
+    "H|0>" in {
+      println("H|0> " + H(S0).state)
+      H(S0).hist
+      true
+    }
+
+    "H|1>" in {
+      println("H|1> " + H(S1).state)
+      true
+    }
+
     "HZH = X" in {
       def HZH(s: QState[Std]): QState[Std] = s >>= H >>= Z >>= H
       //val HZH = H >=> Z >=> H
