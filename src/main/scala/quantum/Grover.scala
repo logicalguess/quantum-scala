112748ee91cf1c12a596d6a591c4d8e4ac324acf
logicalguess <logicalguess@gmail.com>
2018-07-04 17:29:59 -0400

    Grover histograms

--- src/main/scala/quantum/Grover.scala
+++ src/main/scala/quantum/Grover.scala
@@ -6,14 +6,10 @@ import quantum.Gate._
 import scala.language.reflectiveCalls
 
 object Grover {
-  val report: QState[_ <: Labeled] => Unit = s => {
-    println("Iteration state: " + s)
-    s.hist
-  }
 
-  implicit val noSideEffect: Any => Unit = { _ => }
+  val noSideEffect: Any => Unit = { _ => }
 
-  def iterate[A](n: Int, a: A)(f: A => A)(implicit sideEffect: A => Unit): A = {
+  def iterate[A](n: Int, a: A)(f: A => A)(implicit sideEffect: A => Unit = noSideEffect): A = {
     sideEffect(a)
     if (n <= 0) a
     else iterate(n-1, f(a))(f)(sideEffect)
@@ -22,7 +18,7 @@ object Grover {
   /**
    * Grover's algorithm
    */
-  def grover(f: Int => Int, width: Int) = {
+  def grover(f: Int => Int, width: Int)(implicit sideEffect: QState[_ <: Labeled] => Unit = noSideEffect) = {
     val Hn = liftWord(H) _
     val zeroes = pure(Symbol.fromInt(0, width))
     val one = pure(Symbol.fromInt(1, 1))
@@ -35,13 +31,13 @@ object Grover {
     val r = (math.Pi * math.sqrt(math.pow(2, width)) / 4).toInt
     // zeroes * one >>= lift12(Hn, Hn) >>= repeat(r)(inv >=> lift1(refl))
     val init = zeroes * one >>= lift12(Hn, Hn)
-    iterate(r, init)(_ >>= (inv >=> lift1(refl)))(report)
+    iterate(r, init)(_ >>= (inv >=> lift1(refl)))(sideEffect)
   }
 
-  def run(n: Int): Int = {
+  def run(n: Int)(implicit sideEffect: QState[_ <: Labeled] => Unit = noSideEffect): Int = {
     def f(x: Int) = if (x == n) 1 else 0
     val bits = (math.log(n) / math.log(2)).toInt + 1
-    val s = grover(f, bits)
+    val s = grover(f, bits)(sideEffect)
     println("final state: " + s.toString)
     val m = Symbol.toInt(s.measure(_._1).outcome)
     println("measurement: " + m)
--- src/test/scala/quantum/GroverSpec.scala
+++ src/test/scala/quantum/GroverSpec.scala
@@ -6,14 +6,19 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
 class GroverSpec extends FlatSpec with GeneratorDrivenPropertyChecks {
 
+  val report: QState[_ <: Labeled] => Unit = s => {
+    println("Iteration state: " + s)
+    s.hist
+  }
+
+  "Grover algorithm" should "find 5" in  {
+    assert(Grover.run(5)(report) == 5)
+  }
+
   implicit val searchValues: Arbitrary[Int] = Arbitrary {
     for {
       v <- Gen.choose[Int](0, 99)
     } yield v
-  }
-
-  "Grover algorithm" should "find 5" in  {
-    assert(Grover.run(5) == 5)
   }
 
   "Grover algorithm" should "find any value" in forAll  { v: Int =>
