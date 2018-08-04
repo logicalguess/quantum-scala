e5d33f70c0cb0d63ad1eb28e2d963d1cdbb2cd66
logicalguess <logicalguess@gmail.com>
2018-08-03 20:08:44 -0400

    QFT

--- src/main/scala/quantum/algorithm/QFT.scala
+++ src/main/scala/quantum/algorithm/QFT.scala
@@ -6,8 +6,48 @@ import quantum.domain.Labeled.Tensor
 import quantum.domain.QState
 import quantum.domain.Symbol.{Std, Word}
 
-
 object QFT {
+
+  def iqft(qs: List[Int])(s: QState[Word[Std]]): QState[Word[Std]] = {
+    var state = s
+    for (j <- (0 to qs.size - 1).reverse) {
+      state = state >>= wire(qs(j), H)
+      for (k <- (0 to j - 1).reverse)
+        state = state >>= controlledW(qs(j), qs(k), R(-math.Pi / math.pow(2, j - k)))
+    }
+    state
+  }
+
+  def qft1(qs: List[Int])(s: QState[Word[Std]]): QState[Word[Std]] = {
+    var state = s
+    for (j <- (0 to qs.size - 1).reverse) {
+      state = state >>= wire(qs(j), H)
+      for (k <- (0 to j - 1).reverse)
+        state = state >>= controlledW(qs(k), qs(j), R(math.Pi / math.pow(2, j - k)))
+    }
+    state
+  }
+
+  def qftL(qs: List[Int])(s: QState[Word[Std]]): QState[Word[Std]] = {
+    var state = s
+    for (j <- (0 to qs.size - 1)) {
+      state = state >>= wire(qs(j), H)
+      for (k <- (j + 1 to qs.size - 1))
+        state = state >>= controlledW(k, j, R(math.Pi / math.pow(2, k - j)))
+    }
+    state >>= reverse
+  }
+
+  def qft(s: Word[Std]): QState[Word[Std]] = {
+    qftL((0 to s.letters.size - 1).toList)(pure(s))
+//    var state = pure(s)
+//    for (j <- (0 to s.letters.size - 1)) {
+//      state = state >>= wire(j, H)
+//      for (k <- (j + 1 to s.letters.size - 1))
+//        state = state >>= controlledW(k, j, R(math.Pi / math.pow(2, k - j)))
+//    }
+//    state >>= reverse
+  }
 
   // Quantum Fourier Transform
   def QFT(b: Word[Std]): QState[Word[Std]] = {
--- src/test/scala/quantum/algorithm/QFTSpec.scala
+++ src/test/scala/quantum/algorithm/QFTSpec.scala
@@ -2,10 +2,42 @@ package quantum.algorithm
 
 import org.scalatest.FlatSpec
 
+import quantum.domain.QState._
+import quantum.domain.Symbol.Word
+
 class QFTSpec extends FlatSpec {
 
   "QFT algorithm" should "run" in  {
     println(QFT.run)
+  }
+
+  "fourier" should "" in {
+    def qft(qs: List[Int]) = {
+      for (j <- (0 to qs.size - 1).reverse) {
+        println(qs(j))
+        for (k <- (0 to j - 1).reverse)
+          println("\t" + qs(k) + "  R" + (j - k))
+      }
+    }
+
+    qft((1 to 5).toList.reverse)
+  }
+
+  "compare" should "work" in {
+
+    val s = pure(Word.fromInt(0, 4))
+    assert((s >>= QFT.qft) == (s >>= QFT.QFT))
+    assert((s * rhalf >>= QFT.qft) == (s * rhalf >>= QFT.QFT))
+
+    val s1 = pure(Word.fromInt(8, 4))
+    assert((s1 >>= QFT.qft) == (s1 >>= QFT.QFT))
+    assert((s1 * rhalf >>= QFT.qft) == (s1 * rhalf >>= QFT.QFT))
+
+    val s2 = pure(Word.fromInt(0, 4)) + pure(Word.fromInt(8, 4))
+    assert((s2 >>= QFT.qft) == (s2 >>= QFT.QFT))
+    assert((s2 * rhalf >>= QFT.qft) == (s2 * rhalf >>= QFT.QFT))
+
+    assert((s2 >>= QFT.qft) == (QFT.qftL((0 to s.state.head._1.letters.size - 1).toList)(s2)))
   }
 
 }
