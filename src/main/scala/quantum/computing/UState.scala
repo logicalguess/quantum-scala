f64b25ec588af4d136306290a254b5acc3ae25d9
logicalguess <logicalguess@gmail.com>
2018-08-18 23:31:12 -0400

    combine

--- src/main/scala/quantum/computing/UState.scala
+++ src/main/scala/quantum/computing/UState.scala
@@ -2,25 +2,26 @@ package quantum.computing
 
 trait UState[+This <: UState[This, B, V], B, V] {
   val bins: List[(B, V)]
-  val updateRule: ((B, V), B => List[(B, V)]) => List[(B, V)]
-  val normalizeRule: List[(B, V)] => List[(B, V)] = identity
   val zero: V
   val plus: (V, V) => V
 
-  def create(bins: List[(B, V)]): This
-  def normalize(): This = create(normalizeRule(bins))
-
-  def flatMap(f: B => List[(B, V)]): This = {
-    create(normalizeRule(collect(bins.flatMap({ case bv => updateRule(bv, f) }))))
-  }
-
-  def >>=(f: B => List[(B, V)]): This = flatMap(f)
-
-    private def collect(bins: List[(B, V)]): List[(B, V)] = {
-    bins.groupBy(_._1).toList.map {
+  val normalizeRule: List[(B, V)] => List[(B, V)] = identity
+  val combineRule: List[(B, V)] => List[(B, V)] = { bs =>
+    bs.groupBy(_._1).toList.map {
       case (b, vs) => (b, vs.map(_._2).foldLeft(zero)(plus))
     }
   }
+  val updateRule: ((B, V), B => List[(B, V)]) => List[(B, V)]
+
+  def create(bins: List[(B, V)]): This
+
+  def normalize(): This = create(normalizeRule(bins))
+
+  def flatMap(f: B => List[(B, V)]): This = {
+    create(normalizeRule(combineRule(bins.flatMap({ case bv => updateRule(bv, f) }))))
+  }
+
+  def >>=(f: B => List[(B, V)]): This = flatMap(f)
 }
 
 case class ZState[B](bins: List[(B, Double)]) extends UState[ZState[B], B, Double] {
@@ -50,10 +51,10 @@ case class PState[B](bins: List[(B, Double)]) extends UState[PState[B], B, Doubl
   override val plus: (Double, Double) => Double = _ * _
 
   override val updateRule: ((B, Double), B => List[(B, Double)]) => List[(B, Double)] = {
-    case ((b, v), f) =>  f(b).map { case (c, u) => (c, u * v) }
+    case ((b, v), f) => f(b).map { case (c, u) => (c, u * v) }
   }
 
-  override val normalizeRule  = { bs: List[(B, Double)] =>
+  override val normalizeRule = { bs: List[(B, Double)] =>
     val sum = bs.map(_._2).foldLeft(0.0)(_ + _)
     if (sum == 1.0) bins else bs.map {
       case (b, v) => (b, v / sum)
