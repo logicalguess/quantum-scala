1266f3d58812e5f3431e9985ba27c5ea7179e80b
logicalguess <logicalguess@gmail.com>
2018-08-27 13:11:30 -0400

    inv fix

--- src/test/scala/quantum/algorithm/AmplitudeSpec.scala
+++ src/test/scala/quantum/algorithm/AmplitudeSpec.scala
@@ -9,56 +9,6 @@ import quantum.domain.{Gate, QState}
 
 class AmplitudeSpec extends FlatSpec {
 
-  "bernoulli" should "circuit" in {
-
-    val p = 0.3
-    val theta = math.asin(math.sqrt(p))
-
-    val op: Gate[Std, Std] = rot(theta)
-
-    def q(j: Int): Gate[Std, Std] = rot(math.pow(2, j + 1) * theta)
-
-    val n = 4
-    val circuit = Amplitude.run(n, op, q _)
-
-    circuit.probs
-    circuit.hist
-
-    val estimates = Amplitude.estimate(circuit)
-    println(estimates)
-  }
-
-  "bernoulli1" should "circuit" in {
-
-    val p = 0.3
-    val theta = math.asin(math.sqrt(p))
-
-    val op: Gate[Std, Std] = rot(theta)
-
-    def q(j: Int): Gate[Std, Std] = rot(math.pow(2, j + 1) * theta)
-
-    val n_targets = 3
-
-    var state = pure(Word.fromInt(0, n_targets + 1))
-    state = state >>= wire(n_targets, op)
-    for (j <- (0 until n_targets)) {
-      state = state >>= wire(j, H)
-    }
-    state = state >>= Amplitude.lambdaL(List(0, 1, 2), 3)(q) // Amplitude.lambda(q) //
-    state = state >>= QFT.iqftL(List(0, 1, 2)) // Amplitude.iqft //
-
-    state.probs
-    state.hist
-
-    val result = state.measure(w => Word.toInt(Word(w.letters.take(3)))).outcome
-    println(result)
-    println(math.pow(math.sin(result/math.pow(2, 3)), 2))
-
-    val estimates = Amplitude.estimate(state)
-    println(estimates)
-  }
-
-
   "count" should "phase" in {
 
     //def f(x: Int) = true
