9f46fb4c04b50a01378c200eb8301563d4a064d1
logicalguess <logicalguess@gmail.com>
2018-12-25 14:41:28 -0800

    bias

--- src/test/scala/quantum/algorithm/AmplitudeSpec.scala
+++ src/test/scala/quantum/algorithm/AmplitudeSpec.scala
@@ -2,10 +2,10 @@ package quantum.algorithm
 
 import org.scalatest.FlatSpec
 import quantum.algorithm.Grover._
+import quantum.domain.Gate
 import quantum.domain.Gate.{H, I, Z, controlledL, rot, wire, _}
 import quantum.domain.QState.pure
 import quantum.domain.Symbol.{Std, Word}
-import quantum.domain.{Gate, QState}
 
 class AmplitudeSpec extends FlatSpec {
 
@@ -148,6 +148,76 @@ class AmplitudeSpec extends FlatSpec {
     val factor = math.pow(2, n_targets) //2*n_targets //2*n_controls
     println(ints.map { case (i, p) => (factor*math.pow(math.sin(math.Pi * i/math.pow(2, n_controls)), 2), p)})
 
+  }
+
+  def trunc(x: Double, n: Int = 3): Double = {
+    def p10(n: Int, pow: Long = 10): Long = if (n == 0) pow else p10(n - 1, pow * 10)
+
+    if (n < 0) {
+      val m = p10(-n).toDouble
+      math.round(x / m) * m
+    }
+    else {
+      val m = p10(n).toDouble
+      math.round(x * m) / m
+    }
+  }
+  import scala.collection.immutable.ListMap
+  def highToLow(m: Map[_, Double]) = ListMap(m.mapValues(v => trunc(v)).toSeq.sortWith(_._2 > _._2):_*)
+
+  "bias" should "be approximated" in {
+
+    val n_controls = 7
+    val controls = (0 until n_controls).toList
+
+    val n_targets = 0
+    val targets = (0 until n_targets).toList
+
+    val bias = 0.62
+    val theta = 2 * math.asin(math.sqrt(bias))
+    val A: Gate[Std, Std] = Ry(theta)
+    val Q = Ry(2 * theta)
+
+    var state = pure(Word.fromInt(0, n_controls + n_targets + 1))
+
+    state = state >>= wire(n_controls + n_targets, A)
+
+    for (j <- (0 to n_controls + n_targets)) {
+      state = state >>= wire(j, H)
+    }
+
+    for (i <- 0 until n_controls) {
+      for (j <- 1 to math.pow(2, i).toInt)
+        state = state >>= controlledI(i, wire(n_targets + n_controls, Q))
+    }
+    //state.hist
+
+    state = state >>= QFT.iqftL(controls)
+    state.hist
+
+    val frequencies = for {
+      x <- state.state
+    } yield Word.toInt(l => l.take(n_controls))(x._1) -> x._2.norm2
+    println(frequencies.map({ case (i, d) => (i, trunc(d)) }).groupBy(_._1))
+
+    val combinedFrequencies = frequencies.groupBy(_._1).mapValues { l => l.foldLeft(0.0) { case (s, (k, v)) => s + v } }
+    println(highToLow(combinedFrequencies))
+
+    val sinesSquaredOfProportions = for {
+      x <- state.state
+    } yield (trunc(math.pow(math.sin(math.Pi * Word.toInt(l => l.take(n_controls))(x._1) / math.pow(2, n_controls)), 2)) -> x._2.norm2)
+    println(sinesSquaredOfProportions.groupBy(_._1))
+
+    val estimates = sinesSquaredOfProportions.groupBy(_._1).mapValues { l => l.foldLeft(0.0) { case (s, (k, v)) => s + v } }
+    println(highToLow(estimates))
+
+    val top2: ListMap[Double, Double] = highToLow(estimates).take(2).asInstanceOf[ListMap[Double, Double]]
+    val p1 = top2.head._2
+    val p2 = top2.tail.head._2
+    val s = top2.head._1
+    val e = top2.tail.head._1
+
+    println(s"guess: ${s + (e - s) * p2/(p1+p2)}")
   }
 
 }
